39bf927166cb1d5cb1b4163ccff011325ee3cf17
Daniel <danielkibret8@gmail.com>
2025-09-22 00:17:42 +0300

    frontend

---
+++ frontend/src/assets/svg/index.ts
@@ -0,0 +1,5 @@
+export { default as TwitterIcon } from './TwitterIcon';
+export { default as GitHubIcon } from './GitHubIcon';
+export { default as LinkedInIcon } from './LinkedInIcon';
+export { default as DiscordIcon } from './DiscordIcon';
+export { default as NetscanLogoIcon } from './NetscanLogoIcon';
