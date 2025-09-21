c14648ed1158019df6eb3a93a8c237b5a47ea5c2
Daniel <danielkibret8@gmail.com>
2025-09-22 00:39:30 +0300

    UI fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
 import Home from './Pages/Home';
+import Login from './Pages/Login';
+import { Route,Routes } from 'react-router-dom';
+import Profile from './Pages/Profile';
+import Dashboard from './Pages/Dashboard';
 
 const App = () => {
   return (
     <div className="">
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} /> 
+        <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
     </div>
   )
 }
